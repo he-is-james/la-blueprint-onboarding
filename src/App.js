88307037d12030112802b38f0d69f0b564a85b8f
James <he.is.james7@gmail.com>
2021-11-27 14:27:26 -0800

    finished creating new post
    removed file to simplify process, updates displays when new one is created, finishing milestone 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import CreatePost from './components/CreatePost';
 import PostsDisplay from './components/PostsDisplay';
 
 const App = function () {
   return (
-    <>
-      <CreatePost />
-      <PostsDisplay />
-    </>
+    <PostsDisplay />
   );
 };
 
--- src/components/PostsDisplay.js
+++ src/components/PostsDisplay.js
@@ -14,23 +14,63 @@ const base = new Airtable({ apiKey: airtableConfig.apiKey })
 
 const PostsDisplay = function () {
   const [posts, setPosts] = useState([]);
+  const [printer, setPrinter] = useState('');
+  const [print, setPrint] = useState('');
+  const [update, setUpdate] = useState(false);
+
+  const handleSubmit = () => {
+    base('Posts').create([
+      {
+        fields: {
+          Body: print,
+          Author: printer,
+        },
+      },
+    ], (err, records) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach((record) => {
+        console.log(record.getId());
+      });
+    });
+    setPrinter('');
+    setPrint('');
+    setUpdate(true);
+  };
 
   const getPosts = () => {
     base('Posts').select({ view: 'Grid view' }).all()
       .then((records) => {
         setPosts(records);
+        console.log(records);
       });
   };
 
-  useEffect(getPosts, []);
+  useEffect(() => {
+    if (posts.length && !update) {
+      return;
+    }
+    getPosts();
+  }, [update, posts]);
 
-  return posts.map((post) => (
-    <Post
-      key={post.id}
-      author={post.fields.Author}
-      body={post.fields.Body}
-    />
-  ));
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="Printer" value={printer} onChange={(e) => setPrinter(e.target.value)} />
+        <input type="text" placeholder="What's happening" value={print} onChange={(e) => setPrint(e.target.value)} />
+        <button type="button" onClick={handleSubmit}>Print</button>
+      </div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          author={post.fields.Author}
+          body={post.fields.Body}
+        />
+      ))}
+    </>
+  );
 };
 
 export default PostsDisplay;
