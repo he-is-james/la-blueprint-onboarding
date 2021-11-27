8869fd9afabcd89f424c8e3dc7a050785aaa6da1
James <he.is.james7@gmail.com>
2021-11-27 01:38:01 -0800

    initial create post
    tested creating new posts when button is pressed, requires hard refresh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import CreatePost from './components/CreatePost';
 import PostsDisplay from './components/PostsDisplay';
 
 const App = function () {
   return (
-    <PostsDisplay />
+    <>
+      <CreatePost />
+      <PostsDisplay />
+    </>
   );
 };
 
